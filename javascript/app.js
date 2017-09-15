10c427085bfbe157f452db7ba8d8b34b26c530ab
Ben Lacy <ben.lacy@outlook.com>
2017-09-14 20:04:14 -0500

    Updated navbar and coded JS to load new content when it's clicked

--- javascript/app.js
+++ javascript/app.js
@@ -1,35 +1,5 @@
 // -- VARIABLES -- //
-    var subNavObjArray = {
-        home: {
-            subnav0: 'About Us',
-            subnav1: 'Why Dance',
-            subnav2: 'Customer Thoughts',
-            subnav3: 'Community',
-            subnav4: 'Terminology',
-            subnav5: 'FAQ'
-        },
-        classes: {
-            subnav0: 'Young Children',
-            subnav1: 'Preteens & Teens',
-            subnav2: 'Adults',
-            subnav3: 'Charm',
-            subnav4: 'Dress Code',
-            subnav5: 'Performance',
-            subnav6: 'Policies'
-        },
-        album: {
-            subnav0: 'Photos',
-            subnav2: 'Student Accomplishments',
-            subnav3: 'Videos'
-        },
-        myAcct: {
-            subnav0: 'Register',
-            subnav1: 'Shoes',
-            subnav2: 'Leotards & Tights',
-            subnav3: 'Dance Bags',
-            subnav4: 'Miscellaneous'
-        }
-    };
+
 
 // -- FIREBASE -- //
     // Initialize Firebase
@@ -52,38 +22,15 @@
         });
 
 //-- MISC FUNCTIONS --//
-    // 
-    
-
+    // Loads main content section with different HTML
+        function loadContent(){
+            var page = $(this).attr('data-name');
+            console.log(page);
+            $("#main-content").load("./pages/" + page);
+        }
 //-- CLICK LISTENERS --//
-    // Album Page - Switches out image gallery for YT Video embed
-        $("#videos-btn").click(function(){
-            
-        });
-
     // Main Navbar Buttons
-        $(document).on("click", ".main-nav-btns", loadSubNav);
+        $(document).on("click", ".btn-default", loadContent);
 
 //-- API CODE --//
-    // Facebook
-        window.fbAsyncInit = function() {
-        FB.init({
-            appId            : '124854171581749',
-            autoLogAppEvents : true,
-            xfbml            : true,
-            version          : 'v2.10'
-        });
-            FB.AppEvents.logPageView();
-        };
 
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-
-    // Google Maps
-        // KEY: AIzaSyDa5axavfiocQ69O1zpMUGvcs9QjsgQWI8
-        // Link: https://developers.google.com/maps/documentation/javascript/
