acca1f7eb73cbf5deabef2c4b31f0c71d488c0f4
Ben Lacy <ben.lacy@outlook.com>
2017-09-18 14:41:30 -0500

    navbar collapse complete

--- javascript/app.js
+++ javascript/app.js
@@ -15,56 +15,42 @@
 
          // Capture Button Click
         $("#add-user").on("click", function(event) {
-          // prevent page from refreshing when form tries to submit itself
-          event.preventDefault();
+            // prevent page from refreshing when form tries to submit itself
+            event.preventDefault();
 
-          // Capture user inputs and store them into variables
-          var name = $("#name-input").val().trim();
-          var email = $("#email-input").val().trim();
-          var age = $("#age-input").val().trim();
-          var comment = $("#comment-input").val().trim();
+            // Capture user inputs and store them into variables
+            var name = $("#name-input").val().trim();
+            var email = $("#email-input").val().trim();
+            var age = $("#age-input").val().trim();
+            var comment = $("#comment-input").val().trim();
 
-          var newUser = {
-            name: name,
-            email: email,
-            age: age,
-            comment: comment
-      };
+            var newUser = {
+                name: name,
+                email: email,
+                age: age,
+                comment: comment
+            };
 
              // Uploads email data to the database
             database.ref().push(newUser);
 
-                  // Logs everything to console
-            console.log(newUser.name);
-            console.log(newUser.email);
-            console.log(newUser.age);
-            console.log(newUser.comment);
-
-                  // Clears all of the text-boxes
+            // Clears all of the text-boxes
             $("#name-input").val("");
             $("#email-input").val("");
             $("#age-input").val("");
             $("#comment-input").val("");
-          });
+        });
 
-          database.ref().on("child_added"), function(snapshot){
+        database.ref().on("child_added"), function(snapshot){
+            // Store everything into a variable.
+            var name = snapshot.val().name;
+            var email = snapshot.val().email;
+            var age = snapshot.val().age;
+            var comment = snapshot.val().comment;
 
-            console.log(snapshot.val());
+        };
 
-              // Store everything into a variable.
-              var Name = snapshot.val().name;
-              var email = snapshot.val().email;
-              var age = snapshot.val().age;
-              var comment = snapshot.val().comment;
-
-              // Employee Info
-              console.log(name);
-              console.log(email);
-              console.log(age);
-              console.log(comment);
-          };
-
-    // Update Firebase Data
+    // Update Firebase Data on Page Load
         var database = firebase.database();
         database.ref().on("value", function(snapshot){
             // Do Something
@@ -81,7 +67,7 @@
         
 //-- CLICK LISTENERS --//
     // Sub Navbar Buttons
-        $(document).on("click", ".btn-default", loadContent);        
+        $(document).on("click", ".btn-default", loadContent);
 
 //-- API CODE --//
     // Facebook
