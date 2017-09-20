7f14da1f4e339238587e4e78f4d59f6051b3094b
Ben Lacy <ben.lacy@outlook.com>
2017-09-19 20:12:09 -0500

    restoring

--- javascript/app.js
+++ javascript/app.js
@@ -1,49 +1,20 @@
-<<<<<<< HEAD
-// -- VARIABLES -- //
-
-
-// -- FIREBASE -- //
-    // Initialize Firebase
-        var config = {
-            apiKey: "AIzaSyBDk0aru8IkIQsTUQ1uQRYS7SutJrEXjVI",
-            authDomain: "boot-proj-1.firebaseapp.com",
-            databaseURL: "https://boot-proj-1.firebaseio.com",
-            projectId: "boot-proj-1",
-            storageBucket: "",
-            messagingSenderId: "444510611307"
-        };
-        firebase.initializeApp(config);
-
-    // Update Firebase Data
-        var database = firebase.database();
-        database.ref().on("value", function(snapshot){
-            // Do Something
-        }, function(error){
-            console.log("The read failed: " + error.code);
-        });
-
-=======
 $("#welcome-img").toggleClass('hidden');
 var reloadTest = 0;
->>>>>>> 22fd0adf738d8554702aa067ee36352839b7efb9
 //-- MISC FUNCTIONS --//
     // Loads main content section with different HTML
         function loadContent(){
             var page = $(this).attr('data-name');
             $("#main-content").load("./pages/" + page);
-<<<<<<< HEAD
-=======
             if (reloadTest===0){
                 $("#main-page").toggleClass('hidden');
                 $("#welcome-img").toggleClass('hidden');
                 reloadTest++;
             }
->>>>>>> 22fd0adf738d8554702aa067ee36352839b7efb9
         }
         
 //-- CLICK LISTENERS --//
     // Sub Navbar Buttons
-        $(document).on("click", ".btn-default", loadContent);        
+        $(document).on("click", ".sub-nav", loadContent);
 
 //-- API CODE --//
     // Facebook
@@ -64,7 +35,3 @@ var reloadTest = 0;
             js.src = "//connect.facebook.net/en_US/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
-<<<<<<< HEAD
-
-=======
->>>>>>> 22fd0adf738d8554702aa067ee36352839b7efb9
